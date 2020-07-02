5334055035127017cb616a3b0e01c06a4f1a00ed
Hugo <let.hugosilva@gmail.com>
2020-07-02 22:57:22 +0100

    refactor on some components

--- src/Show.js
+++ src/Show.js
@@ -1,30 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { API_URL, API_KEY } from './config';
 import Navigation from './components/Navigation';
 import ShowInfo from './components/ShowInfo';
-import ShowInfoBar from './components/ShowInfoBar';
 import Actors from './components/Actors';
 import Grid from './components/Grid';
 import Spinner from './components/Spinner';
-import { useShowFetch } from './components/hooks/useShowFetch';
 
-const Show = ({ showId }) => {
-  const [show, loading, error] = useShowFetch(showId);
-  console.log('Show:', show);
+class Show extends Component {
+  state = { loading: true };
 
-  if (error) return <div>Something went wrong, it's not you, it's me</div>;
-  if (loading) return <Spinner />;
+  fetchData = async () => {
+    const { showId } = this.props;
+    this.setState({ loading: true, error: false });
 
-  return (
-    <>
-      <Navigation show={show.original_name} />
-      <ShowInfo show={show} />
-      <Grid header="Actors">
-        {show.actors.map((actor) => (
-          <Actors key={actor.credit_id} actor={actor} />
-        ))}
-      </Grid>
-    </>
-  );
-};
+    try {
+      const endpoint = `${API_URL}tv/${showId}?api_key=${API_KEY}`;
+      const result = await (await fetch(endpoint)).json();
+      console.log(result);
+      const creditsEndpoint = `${API_URL}tv/${showId}/credits?api_key=${API_KEY}`;
+      const creditsData = await (await fetch(creditsEndpoint)).json();
+      console.log(creditsData);
+
+      const directors = creditsData.crew.filter(
+        (member) => member.job === 'Director'
+      );
+      this.setState(
+        {
+          ...result,
+          actors: creditsData.cast,
+          directors,
+          loading: false,
+        },
+        () => {
+          localStorage.setItem(showId, JSON.stringify(this.state));
+        }
+      );
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  componentDidMount() {
+    const { showId } = this.props;
+    if (localStorage[showId]) {
+      this.setState(JSON.parse(localStorage[showId]));
+    } else {
+      this.fetchData();
+    }
+  }
+
+  render() {
+    const { original_name: originalName, actors, error, loading } = this.state;
+
+    if (error) return <div>Something went wrong, it's not you, it's me</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <>
+        <Navigation show={originalName} />
+        <ShowInfo show={this.state} />
+        <Grid header="Actors">
+          {actors.map((actor) => (
+            <Actors key={actor.credit_id} actor={actor} />
+          ))}
+        </Grid>
+      </>
+    );
+  }
+}
 
 export default Show;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {
   SEARCH_ENDPOINT,
   SEARCH_POPULAR,
@@ -12,27 +12,68 @@ import ShowThumbnail from './ShowThumbnail';
 import Spinner from './Spinner';
 import LoadMore from './LoadMore';
 import NoImage from '../assets/no-image.png';
-import { useHomeFetch } from './hooks/useHomeFetch';
 import SearchBar from './SearchBar';
 
-const Home = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [
-    {
-      state: { shows, currentPage, totalPages, heroImage },
-      loading,
-      error,
-    },
-    fetchShows,
-  ] = useHomeFetch(searchTerm);
-
-  const searchShows = (search) => {
-    const endpoint = search ? SEARCH_ENDPOINT + search : SEARCH_POPULAR;
-    setSearchTerm(search);
-    fetchShows(endpoint);
+class Home extends Component {
+  state = {
+    shows: [],
+    searchTerm: '',
+    loading: true,
+    error: false,
   };
 
-  const loadMoreShows = () => {
+  fetchShows = async (endpoint) => {
+    this.setState({ loading: true, error: false });
+
+    const { searchTerm } = this.state;
+    const isLoadingMore = endpoint.search('page');
+
+    try {
+      // 2 awaits because I first await the fetch itself and then await when parsed to json because that's also async
+      const data = await (await fetch(endpoint)).json();
+      this.setState(
+        (prev) => ({
+          ...prev,
+          shows:
+            isLoadingMore !== -1
+              ? // append the new shows and keep the old ones in the state, otherwise wipe them and only show the new ones
+                [...prev.shows, ...data.results]
+              : [...data.results],
+          heroImage: prev.heroImage || data.results[0],
+          currentPage: data.page,
+          totalPages: data.total_pages,
+          loading: false,
+        }),
+        () => {
+          if (!searchTerm) {
+            console.log('Writing to session storage');
+            sessionStorage.setItem('homeState', JSON.stringify(this.state));
+          }
+        }
+      );
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  componentDidMount() {
+    if (sessionStorage.homeState) {
+      console.log('Grabbing from Session Storage');
+      this.setState(JSON.parse(sessionStorage.homeState));
+    } else {
+      console.log('Grabbing from API|');
+      this.fetchShows(SEARCH_POPULAR);
+    }
+  }
+
+  searchShows = (search) => {
+    const endpoint = search ? SEARCH_ENDPOINT + search : SEARCH_POPULAR;
+    this.setState({ searchTerm: search });
+    this.fetchShows(endpoint);
+  };
+
+  loadMoreShows = () => {
+    const { searchTerm, currentPage } = this.state;
     const searchEndpoint = `${SEARCH_ENDPOINT}${searchTerm}&page=${
       currentPage + 1
     }`;
@@ -40,42 +81,54 @@ const Home = () => {
 
     const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
 
-    fetchShows(endpoint);
+    this.fetchShows(endpoint);
   };
 
-  if (error) return <div>Something went wrong...</div>;
-  if (!shows[0]) return <Spinner />;
+  render() {
+    const {
+      searchTerm,
+      heroImage,
+      shows,
+      currentPage,
+      totalPages,
+      loading,
+      error,
+    } = this.state;
 
-  return (
-    <>
-      <HeroImage
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-        title={heroImage.original_name}
-        text={heroImage.overview}
-      />
-      <SearchBar callback={searchShows} />
-      <Grid header={searchTerm ? 'Search Result' : 'Popular Shows'}>
-        {shows.map((show) => (
-          <ShowThumbnail
-            key={show.id}
-            clickable
-            image={
-              show.poster_path
-                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${show.poster_path}`
-                : NoImage
-            }
-            showId={show.id}
-            showName={show.original_name}
-          />
-        ))}
-      </Grid>
+    if (error) return <div>Something went wrong...</div>;
+    if (!shows[0]) return <Spinner />;
 
-      {loading && <Spinner />}
-      {currentPage < totalPages && !loading && (
-        <LoadMore text="Load More" callback={loadMoreShows} />
-      )}
-    </>
-  );
-};
+    return (
+      <>
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+          title={heroImage.original_name}
+          text={heroImage.overview}
+        />
+        <SearchBar callback={this.searchShows} />
+        <Grid header={searchTerm ? 'Search Result' : 'Popular Shows'}>
+          {shows.map((show) => (
+            <ShowThumbnail
+              key={show.id}
+              clickable
+              image={
+                show.poster_path
+                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${show.poster_path}`
+                  : NoImage
+              }
+              showId={show.id}
+              showName={show.original_name}
+            />
+          ))}
+        </Grid>
+
+        {loading && <Spinner />}
+        {currentPage < totalPages && !loading && (
+          <LoadMore text="Load More" callback={this.loadMoreShows} />
+        )}
+      </>
+    );
+  }
+}
 
 export default Home;
